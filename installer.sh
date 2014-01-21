f0ae53ed948a8cb9a2259aeaaf184b43524c3186
Ionică Bizău <bizauionica@yahoo.com>
2014-01-21 07:57:12 +0200

    Copy recursive

--- installer.sh
+++ installer.sh
@@ -6,6 +6,6 @@ mkdir ~/.johnnys-webview-apps
 rm -rf ~/.johnnys-webview-apps/webpagent
 mkdir ~/.johnnys-webview-apps/webpagent
 
-cp * ~/.johnnys-webview-apps/webpagent
+cp -r * ~/.johnnys-webview-apps/webpagent
 
 echo "> Finished."
